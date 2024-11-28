01e91ab895c3f89e770b3be6e191fb94ac42d20f
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-11-28 17:27:58 +0000

    Changes for Weather Mobile App - API Login Controller and Login Model

---
+++ garage87/Controllers/API/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using garage87.Helpers;
+using garage87.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace garage87.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IUserHelper _userHelper;
+        public LoginController(IUserHelper userHelper)
+        {
+            _userHelper = userHelper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userHelper.LoginAsync(model);
+
+                var user = await _userHelper.GetUserByEmailAsync(model.Username);
+                bool isAdmin = await _userHelper.IsUserInRoleAsync(user, "Admin");
+                bool isEmployee = await _userHelper.IsUserInRoleAsync(user, "Employee");
+                bool isMechanic = await _userHelper.IsUserInRoleAsync(user, "Mechanic");
+
+                if (isAdmin)
+                {
+                    return Ok(new
+                    {
+                        Success = true,
+                        Role = "Admin"
+                    });
+                }
+                else if (isEmployee)
+                {
+                    return Ok(new
+                    {
+                        Success = true,
+                        Role = "Employee"
+                    });
+                }
+                else if (isMechanic)
+                {
+                    return Ok(new
+                    {
+                        Success = true,
+                        Role = "Mechanic"
+                    });
+                }
+
+                return NotFound(model);
+            }
+
+            return UnprocessableEntity(model);
+        }
+    }
+}
---
+++ garage87/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace garage87.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+    }
+}
